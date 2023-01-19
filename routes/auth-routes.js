c0c7e596a9d705148bbd4ecec64ead67fe7e7eaf
Chaldrak <chaldrakus@gmail.com>
2023-01-19 15:40:20 +0100

    pass avatar on jwt

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -7,15 +7,22 @@ const router = express.Router();
 
 router.post("/signup", async (req, res)=>{
     try {
-        const {username, password} = req.body;
+        const {username, password, confirmPass} = req.body;
+        const avatar = "https://res.cloudinary.com/dumxkdcvd/image/upload/v1673972861/uwp2202806_mu3vgh.jpg";
         if (username.length > 15) {
-            return res.status(400).json({error: "Username too long maximum 15"});
+            return res.status(500).json({error: "Username too long maximum 15"});
+        }
+        if (username.length === 0) {
+            return res.status(500).json({error: "Username must not be empty"});
         }
         if (password.length < 6) {
-            return res.status(400).json({error: "Password must have minimum 6"});
+            return res.status(500).json({error: "Password must have minimum 6"});
+        }
+        if (password !== confirmPass) {
+            return res.status(500).json({error: "The two password don't match"});
         }
         const cryptedPass = await bcrypt.hash(password, 12);
-        const newUser = await pool.query("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *", [username, cryptedPass]);
+        const newUser = await pool.query("INSERT INTO users (username, password, avatar) VALUES ($1, $2, $3) RETURNING *", [username, cryptedPass, avatar]);
         res.status(201).json({user: newUser.rows});
     } catch (error) {
         res.status(500).json({error: error.message});
--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-function jwtTokens({id, username}) {
-    const user = {id, username};
+function jwtTokens({id, username, avatar}) {
+    const user = {id, username, avatar};
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2d'});
     return ({accessToken});
 }
