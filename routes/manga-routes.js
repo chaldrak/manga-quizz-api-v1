a68658dc9e12ce7343a23da4c9ff8f85e7f1f947
Chaldrak <chaldrakus@gmail.com>
2023-02-22 10:49:30 +0100

    last commit before hosting

--- routes/manga-routes.js
+++ routes/manga-routes.js
@@ -17,7 +17,7 @@ router.get("/mangas/:id/characters", authenticateToken, async (req, res)=>{
     try {
         const id = parseInt(req.params.id);
         const characters = await pool.query("SELECT id, name, description, picture FROM ( SELECT c.picture, c.info_id, c.manga_id FROM characters as c JOIN mangas as m ON c.manga_id = m.id  WHERE m.id = $1 ) as res JOIN info_characters as info ON res.info_id = info.id", [id]);
-        res.status(200).json({character: characters.rows});
+        res.status(200).json({characters: characters.rows});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
